a7e013fcf1ec9c1342d10b1c5fe2c3b8c4c0e6af
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-28 03:45:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,15 @@ def game_hash
       colors: ["Black", "White"], 
       players: [
         {
-          player_name: "Alan Anderson", 
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12, 
-          assists: 12, 
-          steals: 3, 
-          blocks: 1, 
-          slam_dunks: 1
+      :player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
         },
         {
           player_name: "Reggie Evans",
